dd0c46274a07b3c2ab0b23ad174fff9a6b06341c
Admin <kawodyathashika@gmail.com>
2026-09-21 15:07:05 +0530

    fix logo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -54,7 +54,7 @@ export default function Footer() {
                         <Link to="/" className="flex items-center gap-2 mb-4">
                             <div className="w-10 h-10 flex items-center justify-center">
                             <img
-                                src="/logo.png"
+                                src="https://raw.githubusercontent.com/KawodyaThashika/Ceylon_Journeys/refs/heads/main/public/logo.png"
                                 alt="Ceylon Journeys"
                                 className="w-10 h-10 object-contain group-hover:scale-105 transition-transform duration-200"
                             />
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar() {
                         <Link to="/" className="flex items-center gap-2 group">
                             <div className="w-10 h-10 flex items-center justify-center">
                                 <img
-                                    src="/logo.png"
+                                    src="https://raw.githubusercontent.com/KawodyaThashika/Ceylon_Journeys/refs/heads/main/public/logo.png"
                                     alt="Ceylon Journeys"
                                     className="w-10 h-10 object-contain group-hover:scale-105 transition-transform duration-200"
                                 />
