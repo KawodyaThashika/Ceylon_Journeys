049e85f97100e1ddaa115cda6c5a18dc16b2214a
Admin <kawodyathashika@gmail.com>
2026-09-22 10:15:32 +0530

    Fix Vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import path from 'path'
 export default defineConfig({
     plugins: [react()],
 
-    base: '/Ceylon_Journeys/',
-
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
